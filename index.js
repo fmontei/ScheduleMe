ca4459c4f85c6aa6c13dc938a0792e702213d7da
Felipe Monteiro <fmontei@g.clemson.edu>
2016-04-17 01:32:39 +0100

    Change app.use('static') to use __dirname for proper lookup.

--- index.js
+++ index.js
@@ -29,15 +29,7 @@ var delete_schedule = require('./routes/delete_schedule');
 
 var app = express();
 
-hbs = ehb.create({
-    defaultLayout: 'main',
-    extname: '.hbs'
-});
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-app.use('/static', express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
